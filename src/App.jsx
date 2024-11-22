79f15f163862da8563ef2bd344c5dd12475beca8
xScafu <a.scarlata93@gmail.com>
2024-11-22 01:43:06 +0100

    backgrounf test unfinished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
+import BackgroundText from "./components/BackgroundText";
 import Navbar from "./components/Navbar";
 import Presentation from "./components/Presentation";
 import Footer from "./components/Footer";
 import { Divider } from "@mui/material";
+import zIndex from "@mui/material/styles/zIndex";
 
 function App() {
   return (
     <>
       <Navbar />
       <Presentation />
+      <BackgroundText />
       <div className="mt-5">
         <Divider>
           <h3>Check my socials</h3>
